f34972710dd352ceb1efc97d4767ad3ccc97ad39
Faethon Milikouris <faethon.milikouris@gmail.com>
2015-11-22 20:07:07 -0800

    Return correct message back to app

--- routes/profile.js
+++ routes/profile.js
@@ -6,8 +6,10 @@ module.exports = function(app) {
   // we will want this protected so you have to be logged in to visit
   // we will use route middleware to verify this (the isLoggedIn function)
   app.get('/profile', isLoggedIn, function(req, res) {
-    res.render('profile.ejs', {
-      user : req.user // get the user out of session and pass to template
+    res.json({
+      // user : req.user // get the user out of session and pass to template
+      email: req.user.email,
+      password: req.user.password
     });
   });
 
